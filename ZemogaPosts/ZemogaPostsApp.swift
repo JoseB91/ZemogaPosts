314c15dbf7e7adb2629f936ae995d24a01f95188
Jose Briones Romero <jose.briones.r@hotmail.com>
2022-07-18 14:50:10 -0500

    Initial Commit

---
+++ ZemogaPosts/ZemogaPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZemogaPostsApp.swift
+//  ZemogaPosts
+//
+//  Created by José Briones Romero on 18/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ZemogaPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
